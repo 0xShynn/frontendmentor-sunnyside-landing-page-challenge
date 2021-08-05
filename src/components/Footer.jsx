237603ce9fd3e4411d3b7a314e9289d80f4415d2
“anhek” <a.nhek@icloud.com>
2021-08-05 19:16:22 +0200

    updated: footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,9 +18,9 @@ const Footer = () => {
       py={{ base: '16' }}
       px="10"
     >
-      <Box w="160px" mb="10">
+      <Link w="160px" href="#" mb="10">
         <SunnySideLogo color="#2D7565" />
-      </Box>
+      </Link>
 
       <HStack spacing="12">
         {Links.slice(0, Links.length - 1).map(link => (
@@ -28,37 +28,50 @@ const Footer = () => {
             color="primary.sunnyGreen.200"
             _hover={{ color: 'white' }}
             key={link + Math.random()}
+            href="#"
           >
             {link}
           </Link>
         ))}
       </HStack>
 
-      <HStack spacing="6" mt="20">
-        <Icon
-          as={IoLogoFacebook}
-          boxSize="6"
-          color="primary.sunnyGreen.300"
-          _hover={{ color: 'white' }}
-        />
-        <Icon
-          as={IoLogoInstagram}
-          boxSize="6"
-          color="primary.sunnyGreen.300"
-          _hover={{ color: 'white' }}
-        />
-        <Icon
-          as={IoLogoTwitter}
-          boxSize="6"
-          color="primary.sunnyGreen.300"
-          _hover={{ color: 'white' }}
-        />
-        <Icon
-          as={IoLogoPinterest}
-          boxSize="6"
-          color="primary.sunnyGreen.300"
-          _hover={{ color: 'white' }}
-        />
+      <HStack spacing="6" mt="20" flexWrap="wrap">
+        <Link href="#">
+          <Icon
+            as={IoLogoFacebook}
+            boxSize="6"
+            color="primary.sunnyGreen.300"
+            _hover={{ color: 'white' }}
+            role="link"
+          />
+        </Link>
+        <Link href="#">
+          <Icon
+            as={IoLogoInstagram}
+            boxSize="6"
+            color="primary.sunnyGreen.300"
+            _hover={{ color: 'white' }}
+            role="link"
+          />
+        </Link>
+        <Link href="#">
+          <Icon
+            as={IoLogoTwitter}
+            boxSize="6"
+            color="primary.sunnyGreen.300"
+            _hover={{ color: 'white' }}
+            role="link"
+          />
+        </Link>
+        <Link href="#">
+          <Icon
+            as={IoLogoPinterest}
+            boxSize="6"
+            color="primary.sunnyGreen.300"
+            _hover={{ color: 'white' }}
+            role="link"
+          />
+        </Link>
       </HStack>
     </Flex>
   );
