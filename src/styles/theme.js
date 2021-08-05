e01826ef0f82de0868537e068623d01449e5085c
“anhek” <a.nhek@icloud.com>
2021-08-05 11:11:48 +0200

    added: customTheme

---
+++ src/styles/theme.js
@@ -0,0 +1,29 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const customTheme = extendTheme({
+  styles: {
+    global: {
+      // styles for the `body`
+      body: {
+        fontSize: '18px',
+      },
+    },
+  },
+  colors: {
+    primary: {
+      softRed: 'hsl(7, 99%, 70%)',
+      yellow: 'hsl(51, 100%, 49%)',
+      darkDesCyan: 'hsl(167, 40%, 24%)',
+      darkBlue: 'hsl(198, 62%, 26%)',
+      darkModCyan: 'hsl(168, 34%, 41%)',
+    },
+    neutral: {
+      'blue.100': 'hsl(212, 27%, 19%)',
+      'blue.200': 'hsl(213, 9%, 39%)',
+      'blue.300': 'hsl(232, 10%, 55%)',
+      'blue.400': 'hsl(210, 4%, 67%)',
+    },
+  },
+});
+
+export default customTheme;
